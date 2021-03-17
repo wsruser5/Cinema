e3ccfc658487b4f65753d8f467a7d37fef0e104c
wsruser5 <maratdzhemankulov@gmail.com>
2021-03-17 15:49:28 +0300

    1

--- app/src/main/java/com/mrz/worldcinema/MainScreen/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/mrz/worldcinema/MainScreen/fragments/home/HomeFragment.kt
@@ -1,13 +1,12 @@
-package com.mrz.worldcinema.MainScreen.ui.home
+package com.mrz.worldcinema.MainScreen.fragments.home
 
+import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.mrz.worldcinema.R
 
 class HomeFragment : Fragment() {
@@ -21,11 +20,8 @@ class HomeFragment : Fragment() {
     ): View? {
         homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        val root = inflater.inflate(R.layout.home_fragment, container, false)
         return root
     }
+
 }
